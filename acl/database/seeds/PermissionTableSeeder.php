0cde5e112b8dbbec9e8d1f987067c7f089177455
Sufyan17 <1167050108@student.uinsgd.ac.id>
2018-12-17 22:07:08 +0700

    acl

---
+++ acl/database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $permission = [
+           'role-list',
+           'role-create',
+           'role-edit',
+           'role-delete',
+           'product-list',
+           'product-create',
+           'product-edit',
+           'product-delete'
+        ];
+
+
+        foreach ($permission as $permission) {
+             Permission::create(['name' => $permission]);
+        }
+    }
+}
